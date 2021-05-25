67081cc735039e9439af10e3af3fde6eaea15220
Ruslan Lavrov <bstretchers@gmail.com>
2021-05-26 02:11:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("keydown", function(e) {
+    if(e.code === "KeyA"){
+        let audio = new Audio("Music/A.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyS"){
+        let audio = new Audio("Music/S.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyD"){
+        let audio = new Audio("Music/D.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyF"){
+        let audio = new Audio("Music/F.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyG"){
+        let audio = new Audio("Music/G.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyH"){
+        let audio = new Audio("Music/H.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyJ"){
+        let audio = new Audio("Music/J.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyW"){
+        let audio = new Audio("Music/W.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyE"){
+        let audio = new Audio("Music/E.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyT"){
+        let audio = new Audio("Music/T.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyY"){
+        let audio = new Audio("Music/Y.mp3");
+        audio.play();
+    }
+    else if(e.code === "KeyU"){
+        let audio = new Audio("Music/U.mp3");
+        audio.play();
+    }
+    else{
+        console.log("What is this");
+    }
+});
