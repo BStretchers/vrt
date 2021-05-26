2920fb311ee41d035fca48ac037384cc01b4aa90
Ruslan Lavrov <bstretchers@gmail.com>
2021-05-27 02:35:29 +0300

    Added color change on presses

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
+let pressedKeys = [];
+let pressedSize;
+let releasedSize;
+let releasedColor;
+let whiteKeys = [ "A", "S", "D", "F", "G", "H", "J" ];
+let blackKeys = [ "W", "E", "T", "Y", "U" ];
+
 document.addEventListener("keydown", function(e) {
-    if(e.code === "KeyA"){
-        let audio = new Audio("Music/A.mp3");
-        audio.play();
+
+    //Checking if the pressed key is a letter and if its one of the right ones
+    let keyCheck = e.code.substring(0,3) === "Key";
+    if(keyCheck == false)
+        return;
+    
+    let key = e.code.substring(3,4);
+
+    if(whiteKeys.includes(key)){
+        pressedSize = "110px";
     }
-    else if(e.code === "KeyS"){
-        let audio = new Audio("Music/S.mp3");
-        audio.play();
+    else if(blackKeys.includes(key)){
+        pressedSize = "76px";
     }
-    else if(e.code === "KeyD"){
-        let audio = new Audio("Music/D.mp3");
-        audio.play();
+    else
+        return;
+
+    //dont add the key if its a doublepress
+    if(pressedKeys.includes(e.code) == false){
+        pressedKeys.push(e.code);
     }
-    else if(e.code === "KeyF"){
-        let audio = new Audio("Music/F.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyG"){
-        let audio = new Audio("Music/G.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyH"){
-        let audio = new Audio("Music/H.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyJ"){
-        let audio = new Audio("Music/J.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyW"){
-        let audio = new Audio("Music/W.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyE"){
-        let audio = new Audio("Music/E.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyT"){
-        let audio = new Audio("Music/T.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyY"){
-        let audio = new Audio("Music/Y.mp3");
-        audio.play();
-    }
-    else if(e.code === "KeyU"){
-        let audio = new Audio("Music/U.mp3");
-        audio.play();
-    }
-    else{
-        console.log("What is this");
+
+    let audio = new Audio("Music/" + key + ".mp3");
+    audio.play();
+    document.getElementById(key).style.backgroundColor = "aqua";
+    document.getElementById(key).style.height = pressedSize;
+});
+
+document.addEventListener("keyup", function(e) {
+    if(pressedKeys.includes(e.code)){
+        pressedKeys.splice(pressedKeys.indexOf(e.code), 1);
+        let key = e.code.substring(3,4);
+
+        if(whiteKeys.includes(key)){
+            releasedSize = "120px";
+            releasedColor = "white";
+        }
+        else{
+            releasedSize = "80px";
+            releasedColor = "black";
+        }
+
+        document.getElementById(key).style.backgroundColor = releasedColor;
+        document.getElementById(key).style.height = releasedSize;
     }
 });
